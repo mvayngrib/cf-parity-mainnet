ba331aca452476683419d03c3cae63ce5f646894
Mark Vayngrib <mark@tradle.io>
2018-09-02 11:51:50 -0400

    refactor: support Authorization header in nginx, sourced from S3 or env vars

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 set -ex
 
-if [ -n "$S3_CONFIG_PATH" ]
+if [ -n "$S3_PATH_TO_API_KEY" ]
 then
-  echo "Using $S3_CONFIG_PATH as proxy config"
-  aws s3 cp $S3_CONFIG_PATH /etc/nginx/nginx.conf
+  echo "Using $S3_PATH_TO_API_KEY as proxy config"
+  export ETH_INDEXER_API_KEY=$(aws s3 cp "s3://$S3_PATH_TO_API_KEY" -)
+  # aws s3 cp $S3_PATH_TO_API_KEY /etc/nginx/conf.d/template.conf
 else
-  echo "\$S3_CONFIG_PATH not set, using default config"
+  export ETH_INDEXER_API_KEY=""
+  echo "\$S3_PATH_TO_API_KEY not set, using default config"
 fi
 
-echo "this is my conf"
+# replaces too many thing (everything with a dollar sign)
+# envsubst < /etc/nginx/conf.d/template.conf > /etc/nginx/nginx.conf
+
+# source:
+# https://github.com/docker-library/docs/issues/496#issuecomment-370452557
+envsubst "`env | awk -F = '{printf \" $$%s\", $$1}'`" < /etc/nginx/conf.d/template.conf > /etc/nginx/nginx.conf
+
 cat /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
