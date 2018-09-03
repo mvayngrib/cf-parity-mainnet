6ca0c73dc0e3b05b6debd97400cd733d8f726ee3
Mark Vayngrib <mark@tradle.io>
2018-09-03 18:54:16 -0400

    feat: add optional bastion host for easy ssh, fix a couple bugs

--- scripts/delete-and-create-stack.sh
+++ scripts/delete-and-create-stack.sh
@@ -7,4 +7,4 @@ source scripts/env.sh
 
 aws --profile "$AWS_PROFILE" cloudformation delete-stack --stack-name "$STACK_NAME"
 aws --profile "$AWS_PROFILE" cloudformation wait stack-delete-complete --stack-name "$STACK_NAME"
-./scripts/create-stack
+./scripts/create-or-update-stack.sh
